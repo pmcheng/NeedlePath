a8dad5e2302fef4dea83934e00e5e9f4c45c2d60
Phillip Cheng <phillip.cheng@usc.edu>
2022-10-29 14:56:15 -0700

    Add project files.

---
+++ NeedlePath/MainForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace NeedlePath
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbTip = new System.Windows.Forms.RadioButton();
+            this.rbTarget = new System.Windows.Forms.RadioButton();
+            this.rbStart = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb.Location = new System.Drawing.Point(602, 134);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(512, 512);
+            this.pb.TabIndex = 0;
+            this.pb.TabStop = false;
+            this.pb.DragDrop += new System.Windows.Forms.DragEventHandler(this.pb_DragDrop);
+            this.pb.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            this.pb.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pb_MouseDown);
+            this.pb.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb_MouseMove);
+            this.pb.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pb_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 106);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(529, 267);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 406);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 240);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(325, 406);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 240);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbTip);
+            this.groupBox1.Controls.Add(this.rbTarget);
+            this.groupBox1.Controls.Add(this.rbStart);
+            this.groupBox1.Location = new System.Drawing.Point(602, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(512, 82);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mouse Left Click";
+            // 
+            // rbTip
+            // 
+            this.rbTip.AutoSize = true;
+            this.rbTip.Location = new System.Drawing.Point(377, 32);
+            this.rbTip.Name = "rbTip";
+            this.rbTip.Size = new System.Drawing.Size(55, 24);
+            this.rbTip.TabIndex = 2;
+            this.rbTip.Text = "Tip";
+            this.rbTip.UseVisualStyleBackColor = true;
+            // 
+            // rbTarget
+            // 
+            this.rbTarget.AutoSize = true;
+            this.rbTarget.Location = new System.Drawing.Point(207, 32);
+            this.rbTarget.Name = "rbTarget";
+            this.rbTarget.Size = new System.Drawing.Size(80, 24);
+            this.rbTarget.TabIndex = 1;
+            this.rbTarget.Text = "Target";
+            this.rbTarget.UseVisualStyleBackColor = true;
+            // 
+            // rbStart
+            // 
+            this.rbStart.AutoSize = true;
+            this.rbStart.Checked = true;
+            this.rbStart.Location = new System.Drawing.Point(22, 32);
+            this.rbStart.Name = "rbStart";
+            this.rbStart.Size = new System.Drawing.Size(69, 24);
+            this.rbStart.TabIndex = 0;
+            this.rbStart.TabStop = true;
+            this.rbStart.Text = "Start";
+            this.rbStart.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "NeedlePath";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "by Phillip Cheng, MD MS";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 684);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pb);
+            this.KeyPreview = true;
+            this.Name = "MainForm";
+            this.Text = "NeedlePath";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbTip;
+        private System.Windows.Forms.RadioButton rbTarget;
+        private System.Windows.Forms.RadioButton rbStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NeedlePath/MainForm.cs
@@ -0,0 +1,312 @@
+﻿using FellowOakDicom;
+using FellowOakDicom.Imaging;
+using Synapse5Lib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Web;
+
+
+namespace NeedlePath
+{
+    public partial class MainForm : Form
+    {
+        Synapse5 syn;
+        List<string> dcmfiles;
+        int dcmidx = 0;
+        DicomFile dcmfile;
+        DicomImage dcmimage;
+        Bitmap bmp;
+        const int radius = 5;
+        const int thick = 2;
+        const double x_mouse_scale = 1;
+        const double y_mouse_scale = 1;
+        int anchor_width, anchor_center;
+        Point anchor;
+        int center, width = 0;
+        bool leftMouseDown;
+
+        double start_x, start_y, start_z=0;
+        double target_x, target_y, target_z=0;
+        double tip_x, tip_y, tip_z;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            new DicomSetupBuilder().RegisterServices(s => s.AddFellowOakDicom().AddImageManager<WinFormsImageManager>()).Build();
+            pb.MouseWheel+= new MouseEventHandler(this.pb_MouseWheel);
+            pb.AllowDrop = true;
+            syn = new Synapse5();
+
+        }
+
+        private void load_dicom()
+        {
+            dcmfile = DicomFile.Open(dcmfiles[dcmidx]);
+            dcmimage = new DicomImage(dcmfile.Dataset);
+            repaint_bg_image();
+            repaint();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad0:
+                    center = 400;
+                    width = 2000;
+                    repaint_bg_image();
+                    break;
+                case Keys.NumPad1:
+                    center = 55;
+                    width = 426;
+                    repaint_bg_image();
+                    break;
+                case Keys.NumPad2:
+                    center = -585;
+                    width = 1800;
+                    repaint_bg_image();
+                    break;
+            }
+        }
+
+
+        private void set_point(Point p)
+        {
+            double px, py, pz;
+
+            double x_pix = dcmfile.Dataset.GetValue<double>(DicomTag.PixelSpacing, 0);
+            double y_pix = dcmfile.Dataset.GetValue<double>(DicomTag.PixelSpacing, 1);
+            double rows = dcmfile.Dataset.GetValue<int>(DicomTag.Rows, 0);
+            double cols = dcmfile.Dataset.GetValue<int>(DicomTag.Columns, 0);
+
+            px = dcmfile.Dataset.GetValue<double>(DicomTag.ImagePositionPatient, 0);
+            px += (float) p.X / pb.Width * cols * x_pix;
+            py = dcmfile.Dataset.GetValue<double>(DicomTag.ImagePositionPatient, 1);
+            py += (float) p.Y / pb.Height * rows * y_pix;
+            pz = dcmfile.Dataset.GetValue<double>(DicomTag.ImagePositionPatient, 2);
+
+            if (rbStart.Checked)
+            {
+                start_x = px;
+                start_y = py;
+                start_z = pz;
+            }
+            if (rbTarget.Checked)
+            {
+                target_x = px;
+                target_y = py;
+                target_z = pz;
+            }
+            if (rbTip.Checked)
+            {
+                tip_x = px;
+                tip_y = py;
+                tip_z = pz;
+            }
+
+            repaint();
+        }
+
+        private void repaint()
+        {
+            double x_pos = dcmfile.Dataset.GetValue<double>(DicomTag.ImagePositionPatient, 0);
+            double y_pos = dcmfile.Dataset.GetValue<double>(DicomTag.ImagePositionPatient, 1);
+            double z_pos = dcmfile.Dataset.GetValue<double>(DicomTag.ImagePositionPatient, 2);
+            double x_pix = dcmfile.Dataset.GetValue<double>(DicomTag.PixelSpacing, 0);
+            double y_pix = dcmfile.Dataset.GetValue<double>(DicomTag.PixelSpacing, 1);
+            double rows = dcmfile.Dataset.GetValue<int>(DicomTag.Rows, 0);
+            double cols = dcmfile.Dataset.GetValue<int>(DicomTag.Columns, 0);
+
+            double form_pix_x = cols * x_pix / pb.Width;
+            double form_pix_y = cols * y_pix / pb.Height;
+            bmp = new Bitmap(pb.Width, pb.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.FillRectangle(Brushes.Transparent, new Rectangle(Point.Empty, bmp.Size));
+                Point p = new Point();
+                if (start_z == z_pos)
+                {
+                    p.X = (int)((start_x - x_pos) / form_pix_x);
+                    p.Y = (int)((start_y - y_pos) / form_pix_y);
+                    g.DrawEllipse(new Pen(Color.Red, thick), p.X - radius, p.Y - radius, 2 * radius, 2 * radius);
+                }
+                if (target_z == z_pos)
+                {
+                    p.X = (int)((target_x - x_pos) / form_pix_x);
+                    p.Y = (int)((target_y - y_pos) / form_pix_y);
+                    g.DrawEllipse(new Pen(Color.Green, thick), p.X - radius, p.Y - radius, 2 * radius, 2 * radius);
+                }
+                if (tip_z == z_pos)
+                {
+                    p.X = (int)((tip_x - x_pos) / form_pix_x);
+                    p.Y = (int)((tip_y - y_pos) / form_pix_y);
+                    g.DrawEllipse(new Pen(Color.Yellow, thick), p.X - radius, p.Y - radius, 2 * radius, 2 * radius);
+                }
+
+                if (pb.Image != null) pb.Image.Dispose();
+                pb.Image = bmp;
+            }
+        }
+
+
+
+        private void pb_MouseWheel(object sender, MouseEventArgs e)
+        {
+            int oldidx = dcmidx;
+            if (e.Delta > 0) dcmidx--;
+            if (e.Delta < 0) dcmidx++;
+            if (dcmidx < 0) dcmidx = 0;
+            if (dcmidx >= dcmfiles.Count) dcmidx = dcmfiles.Count - 1;
+            if (dcmidx != oldidx) load_dicom();
+        }
+
+        private void repaint_bg_image()
+        {
+            if (dcmimage!=null)
+            {
+                if (width>0)
+                {
+                    dcmimage.WindowCenter = center;
+                    dcmimage.WindowWidth = width;
+                } else
+                {
+                    center = (int)dcmimage.WindowCenter;
+                    width = (int)dcmimage.WindowWidth;
+                }
+                bmp = new Bitmap(dcmimage.RenderImage().AsSharedBitmap());
+                if (pb.BackgroundImage!=null) pb.BackgroundImage.Dispose();
+                pb.BackgroundImage = bmp;
+            }
+        }
+
+        private void textBoxLine(string line)
+        {
+            textBox1.Text += line + "\r\n";
+            textBox1.SelectionStart = textBox1.TextLength;
+            textBox1.ScrollToCaret();
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData("UniformResourceLocator") is MemoryStream ms)
+            {
+                e.Effect = DragDropEffects.Copy;
+                return;
+            }
+            if (e.Data.GetDataPresent("Text"))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void pb_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (dcmimage != null)
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    anchor = e.Location;
+                    anchor_width = width;
+                    anchor_center = center;
+                }
+                else
+                {
+                    set_point(e.Location);
+                    leftMouseDown = true;
+                }
+            }
+
+        }
+
+        private void pb_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int dx = e.X - anchor.X;
+                int dy = e.Y - anchor.Y;
+                width = anchor_width - (int)(x_mouse_scale * dx);
+                center = anchor_center + (int)(y_mouse_scale * dy);
+                repaint_bg_image();
+            }
+            else if (leftMouseDown)
+            {
+                set_point(e.Location);
+            }
+
+        }
+
+
+
+        private void pb_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                anchor = Point.Empty;
+            }
+            else if (leftMouseDown)
+            {
+                leftMouseDown = false;
+            }           
+        }
+
+        private void pb_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("Text"))
+            {
+                string datasource = (String)e.Data.GetData("Text");
+                System.Collections.Specialized.NameValueCollection qscoll = HttpUtility.ParseQueryString(datasource);
+
+                String studyUID = qscoll.Get("studyUID");
+                String seriesUID = qscoll.Get("DDSeriesIUID");
+                if (seriesUID == null)
+                {
+                    seriesUID = qscoll.Get("seriesUID");
+                }
+                String objectUID = qscoll.Get("objectUID");
+                if (objectUID == null)
+                {
+                    objectUID = qscoll.Get("DDFirstSOPInstanceUID");
+                }
+                if ((studyUID != null) && (seriesUID != null) && (objectUID != null))
+                {
+                    textBox1.Text = "";
+                    //textBoxLine($"studyUID = {studyUID}");
+                    //textBoxLine($"seriesUID = {seriesUID}");
+                    //textBoxLine($"objectUID = {objectUID}");
+                    Uri site = new Uri(datasource);
+                    string url = site.GetLeftPart(UriPartial.Authority);
+                    if (!syn.SetAccessToken(url))
+                    {
+                        syn.SetAccessTokenEnvironment(url);
+                    }
+                    dynamic result = syn.GetStudyDetails(studyUID);
+
+                    string tempPath = Path.GetTempPath();
+                    dcmfiles = new List<string>();
+
+                    foreach (dynamic s in result.series)
+                    {
+                        if (s.iuid==seriesUID) {
+                            foreach (dynamic t in s.images)
+                            {
+                                string imageUID = (string)t.iuid;
+                                string imageFile = Path.Combine(tempPath, imageUID + ".dcm");
+                                dcmfiles.Add(imageFile);
+                                syn.GetDicom(studyUID, seriesUID, imageUID, imageFile);
+                                if (imageUID == objectUID)
+                                {
+                                    dcmidx = dcmfiles.Count - 1;
+                                }
+                            }
+                        }
+                    }
+                    load_dicom();
+                }
+            }
+        }
+    }
+}
